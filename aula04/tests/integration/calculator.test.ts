6463f34f07c777e039f2ad39fef16f7a4d64de02
Pedro Nascimento <sn.jaypee@gmail.com>
2023-10-02 21:32:38 -0300

    Aula 04

--- aula04/tests/integration/calculator.test.ts
+++ aula04/tests/integration/calculator.test.ts
@@ -1,12 +1,14 @@
 import supertest from "supertest";
 import app from "../../src/app";
 import { MathBody } from "../../src/validator";
+import calculator from "calculator";
 
 const api = supertest(app);
 
 describe("calculator tests", () => {
   it("when missing params, should return status 422", async () => {
-    const mathBody = { // missing properties on purpose
+    const mathBody = {
+      // missing properties on purpose
       operation: "sum",
     };
 
@@ -18,69 +20,34 @@ describe("calculator tests", () => {
     const mathBody = {
       operation: "not",
       n1: 2,
-      n2: 2
+      n2: 2,
     };
 
     const { status } = await api.post("/math").send(mathBody);
     expect(status).toBe(400);
   });
 
-  it("should sum two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "sum",
-      n1: 2,
-      n2: 2
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(4);
+  it("should sum two numbers", () => {
+    const result = calculator.sum(2, 2);
+    expect(result).toBe(4);
   });
 
-  it("should subtract two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "sub",
-      n1: 2,
-      n2: 2
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(0);
+  it("should subtract two numbers", () => {
+    const result = calculator.sub(2, 2);
+    expect(result).toBe(0);
   });
-  it("should multiply two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "mul",
-      n1: 3,
-      n2: 3
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(9);
+  it("should multiply two numbers", () => {
+    const result = calculator.mul(7, 2);
+    expect(result).toBe(14);
   });
 
-  it("should divide two numbers", async () => {
-    const mathBody: MathBody = {
-      operation: "div",
-      n1: 2,
-      n2: 2
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(1);
+  it("should divide two numbers", () => {
+    const result = calculator.div(8, 2);
+    expect(result).toBe(4);
   });
 
-  it("should return 0 when diving by zero", async () => {
-    const mathBody: MathBody = {
-      operation: "div",
-      n1: 2,
-      n2: 0
-    };
-
-    const { status, body } = await api.post("/math").send(mathBody);
-    expect(status).toBe(200);
-    expect(body.result).toBe(0);
+  it("should return 0 when diving by zero", () => {
+    const result = calculator.div(10, 0);
+    expect(result).toBe(0);
   });
 });
