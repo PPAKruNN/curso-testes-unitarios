383d5ecf784ef37e50ef5087e565150e8b7e2879
Pedro Nascimento <sn.jaypee@gmail.com>
2023-10-04 17:49:48 -0300

    Aula 06

--- aula06/tests/unit/orders.test.ts
+++ aula06/tests/unit/orders.test.ts
@@ -6,17 +6,52 @@ import { OrderInput } from "../../src/validator";
 
 describe("Order Service Tests", () => {
   it("should create an order", async () => {
-    // TODO
-    expect(true).toBe(true);
+    const input: OrderInput = {
+      client: "Maria",
+      description: "Teste do mock mock",
+    };
+
+    jest
+      .spyOn(orderRepository, "create")
+      .mockImplementationOnce((order: OrderInput): any => {
+        return { protocol: "Mock string", status: "IN_PREPARATION" };
+      });
+
+    const res = await createOrder(input);
+
+    expect(res).toEqual({
+      protocol: expect.any(String),
+      status: "IN_PREPARATION",
+    });
   });
 
   it("should return an order based on the protocol", async () => {
-    // TODO
-    expect(true).toBe(true);
+    jest
+      .spyOn(orderRepository, "getByProtocol")
+      .mockImplementationOnce((protocol: string): any => {
+        return { protocol: "Mock string", status: "IN_PREPARATION" };
+      });
+
+    const res = await getOrderByProtocol("AAA");
+
+    expect(res).toEqual({
+      protocol: "Mock string",
+      status: "IN_PREPARATION",
+    });
   });
 
   it("should return status INVALID when protocol doesn't exists", async () => {
-    // TODO
-    expect(true).toBe(true);
+    jest
+      .spyOn(orderRepository, "getByProtocol")
+      .mockImplementationOnce((): any => {
+        return undefined;
+      });
+
+    const res = await getOrderByProtocol("AAA");
+
+    expect(res).toEqual({
+      protocol: "AAA",
+      status: "INVALID",
+    });
   });
 });
